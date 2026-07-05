e556aae864ebd70c886f6651127f3822d9f428f7
Leonardo <tankull3@gmail.com>
2026-07-05 02:39:44 -0300

    Implementação de JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-console.log(products);
+const container = document.getElementById("products-container");
+container.innerHTML = "";
+function createProductCard(product){
+
+    return `
+        <article class="product-card card">
+            <div class="product-image">
+                <img src="${product.image}" alt="${product.name}">
+            </div>
+            <div class="product-info">
+                <h3>${product.name}</h3>
+                <p>R$ ${product.price}</p>
+            </div>
+        </article>
+    `;
+}
+products.forEach(function(product){
+
+    container.innerHTML += createProductCard(product);
+
+});
--- js/products.js
+++ js/products.js
@@ -2,14 +2,27 @@ const products = [
 
     {
         name: "Notebook Gamer RTX 4060",
+        image: "./Assets/images/notebookgamer.png",
         price: 7999,
-        discount: 15
+        discount: 15,
     },
 
     {
         name: "Teclado Gamer RGB",
         price: 299,
+        image: "./Assets/images/keyboardgamer.png",
         discount: 45
+    },
+
+    {   
+        name:"Mouse Gamer",
+        price:199,
+        discount:20
+    },
+    {
+        name: "Headset Gamer",
+        price: 499,
+        discount: 30
     }
 
 ];
