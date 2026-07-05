ef7ff640785c27b1eccd93118ac5c2a6c4290c53
Leonardo <tankull3@gmail.com>
2026-07-05 01:49:23 -0300

    Implementação de Produtos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-console.log("Marketplace iniciado!");
+console.log(products);
---
+++ js/products.js
@@ -0,0 +1,15 @@
+const products = [
+
+    {
+        name: "Notebook Gamer RTX 4060",
+        price: 7999,
+        discount: 15
+    },
+
+    {
+        name: "Teclado Gamer RGB",
+        price: 299,
+        discount: 45
+    }
+
+];
